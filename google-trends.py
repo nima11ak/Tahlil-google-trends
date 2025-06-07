bdee2f35efa42ef5a3a2c7b3a674ef3cc7aa54ce
NIMA <140067555+nima11ak@users.noreply.github.com>
2025-06-07 07:13:25 -0700

    Update google-trends.py

--- google-trends.py
+++ google-trends.py
@@ -1,24 +1,24 @@
 from pytrends.request import TrendReq
 import matplotlib.pyplot as plt
 
-#
+#اتصال به گوگل ترندز
 pytrends = TrendReq(hl="fa",tz=270)
 
-#
+#موضوعات مورد بررسی
 keywords = ["همراه اول","ایرانسل","رایتل"]
 
-#
+#گرفتن داده از گوگل ترندز
 pytrends.build_payload(keywords,timeframe="today 3-m",geo='IR')
-#
+#دادهای ترند
 data = pytrends.interest_over_time()
-#
+#حذف ستون
 if 'isPartial' in data.columns:
     data= data.drop(columns=['isPartial'])
 
-#
+#چاپ دادها
 print(data.head())
 
-#
+#رسم نمودار ترند
 plt.figure(figsize=(12,6))
 for keyword in keywords:
     plt.plot(data.index,data[keyword], label=keyword)
